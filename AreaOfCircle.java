6919bfcfa1f5a59530b34c6a4e8f9f8f091506ca
Sumreen Banu H <sumreenbanuh@gmail.com>
2025-11-17 19:28:32 +0530

    Add files via upload
     Create a Method to Find the Area of a Circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,19 @@
+
+//2. Create a Method to Find the Area of a Circle
+
+package org.example.JavaClass;
+
+public class AreaOfCircle {
+
+
+    public static double circleArea(){
+        double radius = 5;
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+    public static void main(String[] args){
+        double result = circleArea(); // Storing the value which is returned
+        System.out.println(result); // printing the result of area
+    }
+
+}
